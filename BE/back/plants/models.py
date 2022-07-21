abf368990d8ca857af183f6b9b68948f761bbf63
cloudsea1106 <cloudsea1106@gmail.com>
2022-07-21 16:08:09 +0900

    [BE] feat : Record plants information

--- BE/back/plants/models.py
+++ BE/back/plants/models.py
@@ -4,21 +4,25 @@ from django.conf import settings
 
 class Plants(models.Model):
     name = models.CharField(max_length=100)
-    hd_code = models.CharField(max_length=6)
     watercycle_spring = models.CharField(max_length=6)
+    watercycle_spring_nm = models.CharField(max_length=100)
     watercycle_summer = models.CharField(max_length=6)
+    watercycle_summer_nm = models.CharField(max_length=100)
     watercycle_autumn = models.CharField(max_length=6)
+    watercycle_autumn_nm = models.CharField(max_length=100)
     watercycle_winter = models.CharField(max_length=6)
-    specl_manage_info = models.CharField(max_length=100, blank=True, null=True)
+    watercycle_winter_nm = models.CharField(max_length=100)
+    specl_manage_info = models.TextField(blank=True)
 
 
 class Myplant(models.Model):
-    name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    otp_code = models.CharField(max_length=6, blank=True, null=True)
-    species = models.ForeignKey(Plants, on_delete=models.PROTECT, null=True, blank=True)
+    otp_code = models.CharField(max_length=6, blank=True)
+    name = models.ForeignKey(Plants, on_delete=models.PROTECT, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='images/', blank=True)
+    # photo = models.ImageField(upload_to='images/', blank=True)
+    photo = models.TextField(blank=True)
     is_connected = models.BooleanField(default=False)
 
 
@@ -32,8 +36,8 @@ class Sensing(models.Model):
 
 class Diary(models.Model):
     content = models.CharField(max_length=1000)
-    photo = models.TextField()
+    photo = models.TextField(blank=True)
     diary_created_at = models.DateTimeField(auto_now_add=True)
     public_private = models.BooleanField(default=False)
-    plant = models.ForeignKey(Myplant, on_delete=models.CASCADE)
+    my_plant = models.ForeignKey(Myplant, on_delete=models.CASCADE)
 
