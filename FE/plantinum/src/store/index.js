223b0a972638cdcf1cc4e041b412eedb82bdf3bd
MoonysKR <dytjd297@gmail.com>
2022-07-21 11:04:14 +0900

    [FE] feat : Set vuex router axios

--- FE/plantinum/src/store/index.js
+++ FE/plantinum/src/store/index.js
@@ -1,4 +1,7 @@
 import { createStore } from 'vuex'
+import { Account } from './modules/accounts'
+
+// import accounts from './modules/accounts'
 
 export default createStore({
   state: {
@@ -9,6 +12,24 @@ export default createStore({
   },
   actions: {
   },
-  modules: {
-  }
+  modules: { Account }
 })
+
+
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+
+// Vue.use(Vuex)
+
+// export default new Vuex.Store({
+//   state: {
+//   },
+//   getters: {
+//   },
+//   mutations: {
+//   },
+//   actions: {
+//   },
+//   modules: {
+//   }
+// })
