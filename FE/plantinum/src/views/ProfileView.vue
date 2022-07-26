befae63cbe832f9115f9499dbd6637a1b6e4db25
MoonysKR <dytjd297@gmail.com>
2022-07-26 22:43:56 +0900

    [FE] feat : Modify profile
    Former-commit-id: 0c9050de807640258185e9c6afd3a2ff5a295bab

--- FE/plantinum/src/views/ProfileView.vue
+++ FE/plantinum/src/views/ProfileView.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
-
+    <div class="profile row">
+      <!-- 여백 -->
+      <div class="col-md-1"></div>
+      <!-- 내용 담을 흰 상자 -->
+      <profile-detail class="col-md-10 mb-5 pb-5"></profile-detail>
+      <!-- 여백 -->
+      <div class="col-md-1"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import ProfileDetail from '@/components/ProfileDetail.vue'
+
 export default {
   name: 'ProfileView',
   components: {
-    
+    ProfileDetail
   }
 }
 </script>
+
+<style scoped>
+.profile {
+  background-image: linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url('@/assets/ProfileView/background_img.jpg');
+  background-size: cover;
+}
+</style>
