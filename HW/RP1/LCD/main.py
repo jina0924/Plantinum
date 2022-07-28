c1d19a980817fa422e512865defd31dd204d2551
jina jeong <jina092400@gmail.com>
2022-07-28 09:14:39 +0100

    [HW] feat : Resize window and fix bug in rp4
    Former-commit-id: ffad51f32f77036a1cb1972cec3859ad0bf0d89e

--- HW/RP1/LCD/main.py
+++ HW/RP1/LCD/main.py
@@ -1,29 +1,40 @@
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
-from entry import Ui_MainWindow
 from entryUI import Ui_Form as Ui_EntryUI
 from mainUI import Ui_Form as Ui_MainUI
 from detailUI import Ui_Form as Ui_DetailUI
 from otpUI import Ui_Form as Ui_Otp
+from sleepUI import Ui_Form as Ui_SleepUI
 
 import mysql.connector
 import json
 from datetime import datetime
 import time
+import sys
 
 
 #variable
 success_acc = 0
 detail_back =0
 otp_back = 0
+
 userfilepath= "./user_setting.json"
 plant_id = -1
+
+#물임계점
 water_point = -1
+#물통 양 디지털
+water_amount = 0
+#recnet_watering의 크기
 watering_cnt=0
 temp=0
 humi=0
-waveLV = 500
+#파도 움직임
+waveLV = 120
+#취침모드
+issleep=0
 
+#센서 측정 스레드_메인시작시 같이 시작
 class sensorThread(QThread):
     global a,success_acc
 
@@ -46,19 +57,24 @@ class sensorThread(QThread):
             else:
                 #code, code
 
-                print("A :",a)
-                a += 1
+                #print("A :",a)
+                #a += 1
+                #센서측정
                 temp += 1
                 humi += 1
+
+                #특정주기 센서
                 if(a>100):
                     a=0
 
-                if ( a% 5 ==0 ):
+                #파도 수위 변경
+                if (a% 5 ==0 ):
                     #waveLV = a*6 + 150
                     watering()
                 time.sleep(1)
 
 
+#물줄때
 def watering():
     global watering_cnt
     user_data['recent_watering'].append(datetime.now().strftime("%y.%m.%d %H:%M"));
@@ -69,9 +85,10 @@ def watering():
     print(*user_data['recent_watering'])
 
     with open(userfilepath, "w", encoding='utf-8') as file:
-        json.dump(user_data, file, indent="\t")
+        json.dump(user_data, file, indent="\t",ensure_ascii=False)
 
 
+#시작페이지
 class EntryPage(QMainWindow, Ui_EntryUI):
     def __init__(self):
         super().__init__()
@@ -79,14 +96,14 @@ class EntryPage(QMainWindow, Ui_EntryUI):
         self.dbset()
 
     def dbset(self):
+        #db커넥트
         global db
         db = mysql.connector.connect(host="localhost",user='root', password='111111', database='testdb',buffered=True)
-        self.cur = db.cursor()
-        self.cur.execute("select * from test")
 
     def main(self):
         pass
 
+    #새식물 등록
     def new_plant(self):
         global otp_back
         #otp 등록페이지로 이동
@@ -94,9 +111,12 @@ class EntryPage(QMainWindow, Ui_EntryUI):
         print("regist new plant!")
         self.go_otp = OtpPage()
         self.go_otp.exec_()
+        #성공적으로 연결시
         if(success_acc == 1):
+            #oldplant통해 메인페이지 진입
             self.old_plant()
         elif (otp_back == 1):
+            #홈버튼 눌러서 돌아올때
             otp_back = 0
             self.show()
 
@@ -108,9 +128,11 @@ class EntryPage(QMainWindow, Ui_EntryUI):
 
 
         #connect test
-        with open(userfilepath, "r") as file:
+        #json 파일로 전에 저장된 정보 불러옴
+        with open(userfilepath, "r", encoding = "utf-8") as file:
             user_data = json.load(file)
 
+        #쿼리요청
         self.cur = db.cursor()
         self.sql_str = "select isconnect from test where id=" + str(user_data['id'])
         self.cur.execute(self.sql_str)
@@ -118,72 +140,83 @@ class EntryPage(QMainWindow, Ui_EntryUI):
         for result in self.cur:
             # connect success 바로 메인페이지로 이동
             if(result[0] == 1):
-                self.hide()
+                #connect == 1 이면 연결유지 상태 메인페이지 이동
                 print("go to main page!")
                 self.second = MainPage()
                 success_acc = 1
                 watering_cnt = len(user_data['recent_watering'])
+                self.cur.close()
+                self.hide()
                 self.second.show()
             else:
+                #연결이 끊기면 오류메세지
                 msgBox = QMessageBox()
                 msgBox.setText("There are no connected plants")
                 msgBox.exec()
 
             print(result[0])
 
-
+        #다시 누를 것 대비_변경반영
+        db.commit()
 #        self.second.exec_()
         #self.close()
         # 메인페이지가 끝나면 바로 종료
 
-
+#메인 페이지 _ 파도 화면
 class MainPage(QDialog, QWidget, Ui_MainUI):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         print("HI I'm MainPage")
-        #self.show()
-        #self.main()
-        #now_timer.start()
+        #시계를 위해 5초에 한번 화면 새로고침
         self.timer = QTimer(self)
         self.timer.setInterval(1000)  # 1초 => 나중에 5초에 한번으로 바꿀거임
         self.timer.timeout.connect(self.show_clock)
-        self.timer.start()
 
+        #센서 스레드
+        #메인화면시작시 스레드 지정 => 이후 계속 동작
         self.snsth = sensorThread()
-        self.snsth.start()
-
         self.warn_label.hide()
 
+        self.main()
+
     def main(self):
-       pass
+        self.timer.start()
+        self.snsth.start()
 
     def go_detailPage(self):
         global detail_back
         #self.hide()
         self.close()
+        #화면 새로고침 필요없음
         self.timer.stop()
+        #디테일페이지 선언 및 화면 전환
         self.detailpage = DetailPage()
         print("go to detail page!")
         self.detailpage.exec_()
+        # 뒤로가기 버튼으로 돌아왔을때
         if(detail_back == 1):
             detail_back =0
             self.show()
             self.timer.start()
 
-    def exit_program(self):
-        print("Bye! - main")
-        self.close()
 
+#    def exit_program(self):
+ #       print("Bye! - main")
+  #      self.close()
+    #화면 새로고침
     def show_clock(self):
         global now_time
-        now = datetime.now().strftime('%H : %M')
+        #시간 00:00 형태
+        self.now = datetime.now().strftime('%H : %M')
         #now_time = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
-        self.clock.setText(now)
-        self.waterLV_label.setText(str(humi))
-        self.temp_label.setText(str(temp))
+        self.clock.setText(self.now)
+        #온습도 글씨
+        self.humi_label.setText(str(humi)+"%")
+        self.temp_label.setText(str(temp)+"ºC")
 
-        self.wave.setGeometry(QRect(0, waveLV , 1024, 600))
+        #파도 조절
+        self.wave.setGeometry(QRect(0, waveLV , 1366, 768))
         #print(now_time)
 
 
@@ -195,16 +228,21 @@ class DetailPage(QDialog, QWidget, Ui_DetailUI):
         super().__init__()
         self.setupUi(self)
         print("HI I'm Detail Page")
-        #self.show()
+        #최근 관수날짜 보드
+        #초기화 후 채워넣음
+        self.watering_board = [self.label_8, self.label_9, self.label_10]
         self.label_8.clear()
         self.label_9.clear()
         self.label_10.clear()
-        self.main()
+        self.label_11.setText(user_data['nickname'])
+        #화면 새로고침 한번 하고 시작
+        self.redo()
+        #self.main()
 
     def main(self):
        #self.testlabel.setText(str(a))
        pass
-
+    #메인페이지로 돌아감
     def go_mainpage(self):
         global detail_back
         print("Bye! - detail")
@@ -213,25 +251,61 @@ class DetailPage(QDialog, QWidget, Ui_DetailUI):
         detail_back = 1
         self.close()
         #self.main.exec_()
-
+    #취침모드
     def sleep_mode(self):
-        self.redo()
-        pass
+        global issleep
 
+        issleep = 1
+        self.close()
+        self.sleeppage = SleepPage()
+        print("go to sleep page!")
+        self.sleeppage.exec_()
+        issleep=0
+        #취침모드에서 벗어나면 바로 메인페이지로 돌아감
+        self.go_mainpage()
+
+    #새로고침
     def redo(self):
         print("redo!!")
-        self.watering_board = [self.label_10,self.label_9,self.label_8]
-
+        #관수정보
         for i in range(len(user_data['recent_watering'])):
-            self.watering_board[i].setText(user_data['recent_watering'][i]);
+            self.watering_board[i].setText(user_data['recent_watering'][len(user_data['recent_watering'])-i-1])
 
-        pass
+        #닉네임
+        self.cur = db.cursor()
+        self.sql_str = "select name from test where id=" + str(user_data['id'])
+        self.cur.execute(self.sql_str)
 
+        for (res) in self.cur:
+            #print(res)
+            #닉네임 변경시
+            if(res[0] != user_data['nickname']):
+                self.label_11.setText(res[0])
+                user_data['nickname'] = res[0]
+                with open(userfilepath, "w", encoding='utf-8') as file:
+                    json.dump(user_data, file, indent="\t", ensure_ascii=False)
+
+        self.cur.close()
+        db.commit()
+
+        #물의 양
+        if(water_amount == 1):
+            self.progressBar.setStyleSheet(
+                "QProgressBar::chunk { background-color : rgb(163, 77, 79) ; border-radius : 10px;} QProgressBar {background-color : rgb(255,255,255);border-radius : 15px;}")
+        else:
+            self.progressBar.setStyleSheet(
+                "QProgressBar::chunk { background-color : rgb(101, 128, 93) ; border-radius : 10px;} QProgressBar {background-color : rgb(255,255,255);border-radius : 15px;}")
+
+    #프로그램 및 라즈베리파이 종료
+    #코드 해제를 위함
     def turnoff(self):
-        pass
+        quit()
+        #라즈베리파이 종료 필요
+        #배쉬파일 연결해야할듯 합니다
+        sys.exit(0)
 
 
-
+#otp코드
 class OtpPage(QDialog, QWidget, Ui_Otp):
     def __init__(self):
         super().__init__()
@@ -248,6 +322,7 @@ class OtpPage(QDialog, QWidget, Ui_Otp):
         print(self.otp_code)
         self.flag=0
 
+        #otp코드가 6자리일때
         if(len(self.otp_code) == 6):
             #self.flag = 1
             self.cur = db.cursor()
@@ -257,13 +332,15 @@ class OtpPage(QDialog, QWidget, Ui_Otp):
             self.cur.execute(self.sql_str)
             # result = 1개
             if(self.cur.rowcount ==1):
-                #id, 닉네임, 물의 양 저장
+                #id, 닉네임, 물의 양 등 저장
                 for(id,name,otp,isconnect) in self.cur:
                     print(id, name, otp, isconnect)
                     #Already connect
+                    #이미 연결이 되어있는 식물의 경우
                     if(isconnect == 1):
                         self.flag = 2
                         break;
+                    #가져온 id(pk),name 저장
                     self.plant_id = int(id)
                     self.name = str(name)
 
@@ -272,25 +349,26 @@ class OtpPage(QDialog, QWidget, Ui_Otp):
                     self.flag = 1
 
                     # make otp=NULL, isconnect = 1
-                    self.sql_str = "update test set otp = "", isconnect = 1 where otp=" + self.otp_code
+                    self.sql_str = "update test set otp = NULL, isconnect = 1 where otp=" + self.otp_code
                     self.cur.execute(self.sql_str)
 
                     #json 수정
-
-                    with open(userfilepath, "r") as file:
+                    #한글 읽기 위하여 encoding 표시
+                    with open(userfilepath, "r", encoding="utf-8") as file:
                         user_data = json.load(file)
 
 
-#                    print(self.user_data['id'], self.user_data['nickname'], self.user_data['water_amount'])
+#                    print(self.user_data['id'], self.user_data['nickname']
                     user_data['id'] = self.plant_id
-                    user_data['water_amount'] = 80
+                    user_data['nickname'] = self.name
                     user_data['recent_watering'] = []
 
+                    #수정반영
                     with open(userfilepath, "w", encoding='utf-8') as file:
-                        json.dump(user_data, file, indent="\t")
+                        json.dump(user_data, file, indent="\t",ensure_ascii=False)
 
                     plant_id = self.plant_id
-                    water_amount = 80
+                    water_amount = 0
 
                     db.commit()
             else:
@@ -309,7 +387,7 @@ class OtpPage(QDialog, QWidget, Ui_Otp):
                 msgBox.setText("Wrong OTP")
             msgBox.exec()
 
-
+            #실패시 otp코드 초기화
             success_acc = 0
             for i in range(6):
                 self.number_label[i].clear()
@@ -323,6 +401,7 @@ class OtpPage(QDialog, QWidget, Ui_Otp):
         otp_back = 1
         self.close()
 
+    #숫자보드 입력
     def click_pad(self):
         self.send = self.sender().text()
         print(self.send)
@@ -340,6 +419,22 @@ class OtpPage(QDialog, QWidget, Ui_Otp):
                 self.number_label[len(self.otp_code)-1].setText(self.send)
 
 
+#취침 화면
+class SleepPage(QDialog, QWidget, Ui_SleepUI):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        print("HI I'm Detail Page")
+        #self.show()
+        self.main()
+
+    def main(self):
+       #self.testlabel.setText(str(a))
+       #ScreenSaver
+       pass
+
+    def wakeup(self):
+        self.close()
 
 
 app=QApplication()
