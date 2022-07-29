ea231b3c11cdb246170e4df680d6a7267e0f3151
cloudsea1106 <cloudsea1106@gmail.com>
2022-07-29 10:52:54 +0900

    [BE] Fix : Fix response error
    Former-commit-id: 786a54ed5a10c3119f0fc9193d021211f847faec

--- BE/back/plants/urls.py
+++ BE/back/plants/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.plants),
     path('search/<plantname>/', views.search),
     path('myplant/', views.create_myplant),
-    path('myplant/<usernickname>/', views.read_myplant),
+    path('myplant/<username>/', views.read_myplant),
     path('myplant/<int:myplant_pk>/otp/', views.create_otp),
     path('myplant/<int:myplant_pk>/disconnect/', views.disconnect),
     path('myplant/<int:myplant_pk>/detail/', views.detail),
