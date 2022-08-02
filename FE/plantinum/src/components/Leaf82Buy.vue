a540b273a806ca2a87dbf168e4e04daaadcb9b59
jina jeong <jina092400@gmail.com>
2022-08-03 08:37:47 +0900

    [FE] feat : New leaf82
    Former-commit-id: b8d1857e0bf679e075c1ec8af71ded576c07ab4e

---
+++ FE/plantinum/src/components/Leaf82Buy.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Leaf82Buy'
+}
+</script>
+
+<style>
+
+</style>
---
+++ FE/plantinum/src/components/Leaf82SearchBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Leaf82SearchBar'
+}
+</script>
+
+<style>
+
+</style>
