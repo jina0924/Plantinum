1a8fc597940293abcdde890f918bcfce042b7312
unknown <dytjdkorea@naver.com>
2022-08-03 08:37:47 +0900

    [FE] feat : New leaf82
    Former-commit-id: b8d1857e0bf679e075c1ec8af71ded576c07ab4e

--- FE/plantinum/src/views/Leaf82View.vue
+++ FE/plantinum/src/views/Leaf82View.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <leaf-82-search-bar></leaf-82-search-bar>
+    <leaf-82-sell></leaf-82-sell>
+    <leaf-82-buy></leaf-82-buy>
+  </div>
 </template>
 
 <script>
+import Leaf82SearchBar from '@/components/Leaf82SearchBar.vue'
+import Leaf82Buy from '@/components/Leaf82Buy.vue'
+import Leaf82Sell from '@/components/Leaf82Sell.vue'
+
 export default {
   name: 'Leaf82View',
+  components: {
+    Leaf82SearchBar,
+    Leaf82Buy,
+    Leaf82Sell,
+  },
+
 }
 </script>
 
