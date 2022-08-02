f589391c501c226de4fdcc0fb8b163c1e4a895ed
jina jeong <jina092400@gmail.com>
2022-08-03 00:57:12 +0900

    [BE] feat : Create and Read leaf82
    Former-commit-id: 9b126e0d532baece04a204bf9d303a6d6c4ee46f

--- BE/back/leaf82/urls.py
+++ BE/back/leaf82/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    
+    path('', views.read_leaf82),
+    path('<sido>/<sigungu>/', views.sido_sigungu_leaf82),
+    path('new/', views.create_leaf82),
+    path('search/sido/', views.search_sido),
+    path('search/<sido>/sigungu/', views.search_sigungu),
 ]
--- BE/back/leaf82/views.py
+++ BE/back/leaf82/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .serializers import JusoSidoSerializer, JusoSigunguSerializer, Leaf82Serializer
+from .models import Juso, Leaf82
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+# 잎팔이 글 작성
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def create_leaf82(request):
+    sido = request.data['sido']
+    sigungu = request.data['sigungu']
+    addr = get_object_or_404(Juso, sido=sido, sigungu=sigungu)
+
+    user = request.user
+
+    serializer = Leaf82Serializer(data=request.data)
+        
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=user, addr=addr)
+        return Response(serializer.data)
+
+
+# 시도(지역) 검색
+@api_view(['GET'])
+def search_sido(request):
+    data = Juso.objects.all()
+    sido = []
+    for s in data:
+        sido_name = {
+            "sido": s.sido
+        }
+
+        if sido and sido[-1] == sido_name:
+            continue
+        sido.append(sido_name)
+
+    return Response(sido)
+
+
+# 시군구(동네) 검색
+@api_view(['GET'])
+def search_sigungu(request, sido):
+    sigungu = Juso.objects.filter(sido=sido)
+    serializer = JusoSigunguSerializer(sigungu, many=True)
+    return Response(serializer.data)
+
+
+# 잎팔이 전체 글 조회 (지역 상관 X)
+@api_view(['GET'])
+def read_leaf82(request):
+
+    leaves = Leaf82.objects.all().order_by('-pk')
+    serializer = Leaf82Serializer(leaves, many=True)
+    return Response(serializer.data)
+
+
+# 잎팔이 특정 지역/동네 글 조회
+@api_view(['GET'])
+def sido_sigungu_leaf82(request, sido, sigungu):
+
+    sido_sigungu = Juso.objects.filter(sido=sido, sigungu__contains=sigungu)
+    
+    leaves = Leaf82.objects.filter(addr__in=sido_sigungu).order_by('-pk')
+    serializer = Leaf82Serializer(leaves, many=True)
+    return Response(serializer.data)
