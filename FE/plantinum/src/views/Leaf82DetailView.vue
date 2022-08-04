0a0238e335d15b2e5997378059fbc8458bdbfd5d
MoonysKR <dytjd297@gmail.com>
2022-08-04 21:45:33 +0900

    [FE] feat : No css leaf82detail
    Former-commit-id: f8511a8821c45800698d27eaabbeca2819da8d82

---
+++ FE/plantinum/src/views/Leaf82DetailView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="leaf82-detail-view">
+    <leaf-82-detail></leaf-82-detail>
+  </div>
+</template>
+
+<script>
+import Leaf82Detail from '@/components/Leaf82Detail.vue'
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Leaf82DetailView',
+  components: {
+    Leaf82Detail
+  },
+  methods: {
+    ...mapActions(['fetchLeaf82Detail'])
+  },
+  created() {
+    this.fetchLeaf82Detail()
+  }
+}
+</script>
+
+<style>
+
+</style>
