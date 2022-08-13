2dd23c8f91934ddf1937a858f79f5dc0ee35aa49
cloudsea1106 <cloudsea1106@gmail.com>
2022-08-13 23:20:53 +0900

    [BE] feat : Delete myplant image and update leaf82 query params and new model
    Former-commit-id: 1dd9855a85cb3dca245edd957dd28fc0055885ae

--- BE/back/leaf82/models.py
+++ BE/back/leaf82/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Juso(models.Model):
@@ -13,18 +14,18 @@ class Leaf82(models.Model):
         ('분양받아요', '분양받아요')
     )
     STATUS_CHOICES = (
-        ('판매중', '판매중'),
-        ('거래완료', '거래완료'),
-        ('예약중', '예약중')
+        ('분양대기', '분양대기'),
+        ('분양완료', '분양완료'),
+        ('분양예약', '분양예약')
     )
 
     plantname = models.CharField(max_length=20)
     photo = models.ImageField(upload_to='images/leaf82/', default='static/monstera.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=1000)
-    price = models.IntegerField()
+    price = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10000000)])
     category_class = models.CharField(max_length=5, choices=CATEGORY_CHOICES)
-    status_class = models.CharField(max_length=4, choices=STATUS_CHOICES, default='판매중')
+    status_class = models.CharField(max_length=4, choices=STATUS_CHOICES, default='분양대기')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     addr = models.ForeignKey(Juso, on_delete=models.PROTECT)
     posting_addr = models.IntegerField(default=0)
