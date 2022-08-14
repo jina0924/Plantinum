90ba955ddffa691191cfc4c80156a55095621c05
jina jeong <jina092400@gmail.com>
2022-08-15 00:56:32 +0900

    [FE] feat : Add leaf82 chat view
    Former-commit-id: 07e92a523e58333651c39f5ef819110c597750e7

--- FE/plantinum/src/store/index.js
+++ FE/plantinum/src/store/index.js
@@ -3,13 +3,14 @@ import { Account } from './modules/accounts'
 import { Myplant } from './modules/myplant'
 import { Leaf82 } from './modules/leaf82'
 import { Timer } from './modules/timer'
+import { Messenger } from './modules/messenger'
 import createPersistedState from "vuex-persistedstate"
 
 // import accounts from './modules/accounts'
 
 export default createStore({
   modules: { 
-    Account, Myplant, Leaf82, Timer,
+    Account, Myplant, Leaf82, Timer, Messenger,
   },
   plugins: [createPersistedState({
     // paths: ['Account', 'Myplant',]
