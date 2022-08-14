272dc7ecbaafbf33c63e26c02dc71c2c73019fff
jina jeong <jina092400@gmail.com>
2022-08-14 15:27:35 +0900

    [FE] feat : Real final leaf82
    Former-commit-id: 070a84a59dca335041b3d0c405f1a273ff8f6e3c

--- FE/plantinum/src/views/Leaf82View.vue
+++ FE/plantinum/src/views/Leaf82View.vue
@@ -1,89 +1,33 @@
 <template>
   <div>
     <nav-bar></nav-bar>
-    <leaf-82-search-bar :limit="limit" ></leaf-82-search-bar>
-    <leaf-82-list :sellItems="sellItems" :buyItems="buyItems"></leaf-82-list>
-    <!-- <leaf-82-list></leaf-82-list> -->
-    <div class="d-flex justify-content-center">
-      더보기
-    </div>
+    <leaf-82-search-list></leaf-82-search-list>
   </div>
 </template>
 
 <script>
-import Leaf82SearchBar from '@/components/Leaf82SearchBar.vue'
-import Leaf82List from '@/components/Leaf82List.vue'
+import Leaf82SearchList from'@/components/Leaf82SearchList.vue'
 import NavBar from '@/components/NavBar.vue'
-import { mapActions , mapGetters } from 'vuex'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Leaf82View',
   components: {
     NavBar,
-    Leaf82SearchBar,
-    Leaf82List,
+    Leaf82SearchList
   },
   data(){
     return {
-      sellItems: [],
-      buyItems: [],
-      limit: 2,
-      loadPage: 1,
-      searchPage: 1
     }
   },
   methods: {
-    ...mapActions(['fetchLeaf82', 'fetchSido']),
-    sortList(items) {
-      this.sellItems = []
-      this.buyItems = []
-      for (let item of items.results) {
-        if (item.category_class === '분양해요') {
-          this.sellItems.push(item)
-        } else {
-          this.buyItems.push(item)
-        }
-      }
-    },
-    trimItems(sellItems, buyItems) {
-      for (let item of sellItems) {
-        const price = Number(item.price)
-        item.price = price.toLocaleString('ko-KR')
-        if (item.plantname.length > 7) {
-          const plantname = item.plantname.substr(0, 7) + '...'
-          item.plantname = plantname
-        }
-      }
-      for (let item of buyItems) {
-        const price = Number(item.price)
-        item.price = price.toLocaleString('ko-KR')
-        if (item.plantname.length > 7) {
-          const plantname = item.plantname.substr(0, 7) + '...'
-          item.plantname = plantname
-        }
-      }
-    },
-    beforeFetchLeaf82() {
-      const params = {
-        page: 1,
-        limit: 2,
-      }
-      this.fetchLeaf82(params)
-    }
+    ...mapActions(['fetchSido']),
   },
   computed: {
-    ...mapGetters(['searchList'])
   },
   created() {
-    this.beforeFetchLeaf82()
     this.fetchSido()
   },
-  watch: {
-    searchList: function() {
-      this.sortList(this.searchList)
-      this.trimItems(this.sellItems, this.buyItems)
-    }
-  }
 }
 </script>
 
