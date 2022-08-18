f46ce5e00c2144e2c9986176e6297b145d738ce3
jina jeong <jina092400@gmail.com>
2022-08-19 06:29:30 +0900

    [BE] docs : Update docs
    Former-commit-id: ce76f4e398618b7c2cd317d70cf2ba15151ca701

--- BE/back/plants/urls.py
+++ BE/back/plants/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.plants),
-    # path('all', views.MemoList.as_view()),
     path('search/', views.search_all),
     path('search/<plantname>/', views.search),
     path('myplant/', views.create_myplant),
