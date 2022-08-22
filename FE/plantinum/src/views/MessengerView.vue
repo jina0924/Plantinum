4d5c2463900a585172695113decc438ef5eefd98
jina0924 <jinajung73@naver.com>
2022-08-22 21:20:00 +0900

    [FE] fix : Update chat message with no blank
    Former-commit-id: e065035384ff8bde4cebef3431435488a37955b4

--- FE/plantinum/src/views/MessengerView.vue
+++ FE/plantinum/src/views/MessengerView.vue
@@ -187,7 +187,7 @@ export default {
       if(this.now_receiver == -1){
         return;
       }
-      if (this.message !== '') {
+      if (this.message.trim() !== '') {
         this.socket.emit('send', {receiver:this.now_receiver , msg: this.message, room_num:this.rooms[this.now_receiver]});
         this.message = ''
       }
