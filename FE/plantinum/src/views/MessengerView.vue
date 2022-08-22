a18ae89e52cba782cd4ca656ac615db3b9d40203
jina jeong <jina092400@gmail.com>
2022-08-22 21:08:03 +0900

    [FE] feat : Fix chat error
    Former-commit-id: d656babc9bfc9cd25f71bfc7b3b446e699c5ecce

--- FE/plantinum/src/views/MessengerView.vue
+++ FE/plantinum/src/views/MessengerView.vue
@@ -187,8 +187,10 @@ export default {
       if(this.now_receiver == -1){
         return;
       }
-      this.socket.emit('send', {receiver:this.now_receiver , msg: this.message, room_num:this.rooms[this.now_receiver]});
-      this.message = ''
+      if (this.message !== '') {
+        this.socket.emit('send', {receiver:this.now_receiver , msg: this.message, room_num:this.rooms[this.now_receiver]});
+        this.message = ''
+      }
     },
 
     start(){
